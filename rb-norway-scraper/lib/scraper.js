13a68d43cab5418c316219ef4a136acd32da8894
harounb <dekuharoun@gmail.com>
2015-03-04 12:45:56 +0000

    Finally doing some scraping of provided html string
    Made a sample html string from rbnorway's TTT2 Jin section. Removed all
    his moves except for 1 to check.

--- rb-norway-scraper/lib/scraper.js
+++ rb-norway-scraper/lib/scraper.js
@@ -1,12 +1,46 @@
 /*global module, require*/
-module.exports = (function scraper(){
+module.exports = (function scraper() {
     'use strict';
+
     var cheerio = require("cheerio"), //Dependencies
-    init; //Functions
+        init, getData, generateData, //Functions
+        $, data; //Variables
 
     init = function init(htmlDocument) {
-        //TODO
+        $ = cheerio.load(htmlDocument);
+        data = {};
+        data.moves = generateData();
+
     };
 
-    return{init: init};
+    generateData = function generateData() {
+        var moves = [],
+            headings = ['notation', 'hit_level', 'damage', 'speed', 'on_block', 'on_hit', 'on_ch'];
+
+        //Converting table to a nested array
+        $('tr').each(function(i) {
+            if (i !== 0) { //Not including first row as it's a heading
+                var move = {};
+                $(this).children().each(function(i, elem) {
+                    move[headings[i]] = $(elem).text();
+                });
+                moves.push(move);
+
+            }
+        });
+        return moves;
+
+    };
+
+    getData = function getData() {
+        return data;
+
+    };
+
+    return {
+        init: init,
+        getData: getData
+
+    };
+
 }());
--- rb-norway-scraper/spec/scraper-spec.js
+++ rb-norway-scraper/spec/scraper-spec.js
@@ -7,6 +7,25 @@
         it("returns an object", function() {
             expect(scraper).toEqual(jasmine.any(Object));
         });
+        describe("and initing it with some html", function() {
+            var html = '<html><body><table border="1"> <tr BGCOLOR="LightGray"> <td><b>Command</b></td> <td><b>Hit level</b></td> <td><b>Damage</b></td> <td><b>Start up frame</b></td> <td><b>Block frame</b></td> <td><b>Hit frame</b></td> <td><b>Counter hit frame</b></td> </tr> </h3> <tr> <td>1</td> <td>h</td> <td>9</td> <td>10</td> <td>+1</td> <td>+8</td> <td>+8</td> </tr> </table></body></html>';
+            scraper.init(html);
+            it("will return a data object when calling it's getData function", function() {
+                expect(scraper.getData()).toEqual(jasmine.any(Object));
+            });
+            it("will return a data object containing move data of valid format", function() {
+                expect(scraper.getData()).toEqual({
+                    moves: [{
+                        notation: '1',
+                        hit_level: 'h',
+                        damage: '9',
+                        speed: '10',
+                        on_block: '+1',
+                        on_hit: '+8',
+                        on_ch: '+8'
+                    }]
+                });
+            });
+        });
     });
-
 }());
