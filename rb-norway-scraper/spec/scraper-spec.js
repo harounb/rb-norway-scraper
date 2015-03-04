9caf0126d09dc44f891597d43cece5305e6c5f66
harounb <dekuharoun@gmail.com>
2015-03-04 17:33:36 +0000

    Added a test for multiple moves
    Added a test for multiple moves and fixed some funny business going on.
    For some reason describe functions were sharing resources before I split
    them out. More research needed.

--- rb-norway-scraper/spec/scraper-spec.js
+++ rb-norway-scraper/spec/scraper-spec.js
@@ -7,13 +7,16 @@
         it("returns an object", function() {
             expect(scraper).toEqual(jasmine.any(Object));
         });
-        describe("and initing it with some html", function() {
-            var html = '<html><body><table border="1"> <tr BGCOLOR="LightGray"> <td><b>Command</b></td> <td><b>Hit level</b></td> <td><b>Damage</b></td> <td><b>Start up frame</b></td> <td><b>Block frame</b></td> <td><b>Hit frame</b></td> <td><b>Counter hit frame</b></td> </tr> </h3> <tr> <td>1</td> <td>h</td> <td>9</td> <td>10</td> <td>+1</td> <td>+8</td> <td>+8</td> </tr> </table></body></html>';
-            scraper.init(html);
+        describe("and initing it with some html containing a single move", function() {
             it("will return a data object when calling it's getData function", function() {
+                var html = '<html><body><table border="1"> <tr BGCOLOR="LightGray"> <td><b>Command</b></td> <td><b>Hit level</b></td> <td><b>Damage</b></td> <td><b>Start up frame</b></td> <td><b>Block frame</b></td> <td><b>Hit frame</b></td> <td><b>Counter hit frame</b></td> </tr> </h3> <tr> <td>1</td> <td>h</td> <td>9</td> <td>10</td> <td>+1</td> <td>+8</td> <td>+8</td> </tr> <tr BGCOLOR="LightGray"> <td>1, 2</td> <td>h, h</td> <td>9,16</td> <td>10</td> <td>-2</td> <td>+3</td> <td>+7</td> </tr> <tr> <td>1, 2, 3</td> <td>h, h, m</td> <td>9,16,27</td> <td>10</td> <td>+2~+3</td> <td>KND</td> <td>KND</td> </tr> <tr BGCOLOR="LightGray"> <td>1, 2, 4</td> <td>h, h, h</td> <td>9,16,40</td> <td>10</td> <td>-4</td> <td>KND</td> <td>KND</td> </tr> <tr> <td>1, 3</td> <td>h, h</td> <td>9,13</td> <td>10</td> <td>-6</td> <td>+4</td> <td>+4</td> </tr></table></body></html>';
+
+                scraper.init(html);
                 expect(scraper.getData()).toEqual(jasmine.any(Object));
             });
             it("will return a data object containing move data of valid format", function() {
+                var html = '<html><body><table border="1"> <tr BGCOLOR="LightGray"> <td><b>Command</b></td> <td><b>Hit level</b></td> <td><b>Damage</b></td> <td><b>Start up frame</b></td> <td><b>Block frame</b></td> <td><b>Hit frame</b></td> <td><b>Counter hit frame</b></td> </tr> </h3> <tr> <td>1</td> <td>h</td> <td>9</td> <td>10</td> <td>+1</td> <td>+8</td> <td>+8</td> </tr> </table></body></html>';
+                scraper.init(html);
                 expect(scraper.getData()).toEqual({
                     moves: [{
                         notation: '1',
@@ -27,5 +30,64 @@
                 });
             });
         });
+
+        describe("and initing with html containing multiple moves", function() {
+            it("will return a data object containing move data of valid format", function() {
+                var html = '<html><body><table border="1"> <tr BGCOLOR="LightGray"> <td><b>Command</b></td> <td><b>Hit level</b></td> <td><b>Damage</b></td> <td><b>Start up frame</b></td> <td><b>Block frame</b></td> <td><b>Hit frame</b></td> <td><b>Counter hit frame</b></td> </tr> </h3> <tr> <td>1</td> <td>h</td> <td>9</td> <td>10</td> <td>+1</td> <td>+8</td> <td>+8</td> </tr> <tr BGCOLOR="LightGray"> <td>1, 2</td> <td>h, h</td> <td>9,16</td> <td>10</td> <td>-2</td> <td>+3</td> <td>+7</td> </tr> <tr> <td>1, 2, 3</td> <td>h, h, m</td> <td>9,16,27</td> <td>10</td> <td>+2~+3</td> <td>KND</td> <td>KND</td> </tr> <tr BGCOLOR="LightGray"> <td>1, 2, 4</td> <td>h, h, h</td> <td>9,16,40</td> <td>10</td> <td>-4</td> <td>KND</td> <td>KND</td> </tr> <tr> <td>1, 3</td> <td>h, h</td> <td>9,13</td> <td>10</td> <td>-6</td> <td>+4</td> <td>+4</td> </tr></table></body></html>';
+
+                scraper.init(html);
+
+                var expectedMoves = {
+                    moves: [{
+                        notation: '1',
+                        hit_level: 'h',
+                        damage: '9',
+                        speed: '10',
+                        on_block: '+1',
+                        on_hit: '+8',
+                        on_ch: '+8'
+                    },{
+                        notation: '1, 2',
+                        hit_level: 'h, h',
+                        damage: '9,16',
+                        speed: '10',
+                        on_block: '-2',
+                        on_hit: '+3',
+                        on_ch: '+7'
+                    },{
+                        notation: '1, 2, 3',
+                        hit_level: 'h, h, m',
+                        damage: '9,16,27',
+                        speed: '10',
+                        on_block: '+2~+3',
+                        on_hit: 'KND',
+                        on_ch: 'KND'
+                    },{
+                        notation: '1, 2, 4',
+                        hit_level: 'h, h, h',
+                        damage: '9,16,40',
+                        speed: '10',
+                        on_block: '-4',
+                        on_hit: 'KND',
+                        on_ch: 'KND'
+                    },{
+                        notation: '1, 3',
+                        hit_level: 'h, h',
+                        damage: '9,13',
+                        speed: '10',
+                        on_block: '-6',
+                        on_hit: '+4',
+                        on_ch: '+4'
+                    }]
+                };
+                expect(scraper.getData().moves[0]).toEqual(expectedMoves.moves[0]);
+                expect(scraper.getData().moves[1]).toEqual(expectedMoves.moves[1]);
+                expect(scraper.getData().moves[2]).toEqual(expectedMoves.moves[2]);
+                expect(scraper.getData().moves[3]).toEqual(expectedMoves.moves[3]);
+                expect(scraper.getData().moves[4]).toEqual(expectedMoves.moves[4]);
+
+            });
+        });
+
     });
 }());
