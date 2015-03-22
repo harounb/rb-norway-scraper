eb200c8f02872528033222e15fecb906d531a53b
Haroun Butt <dekuharoun@gmail.com>
2015-03-22 10:40:36 +0000

    Some formatting changes

--- rb-norway-scraper/spec/requester-spec.js
+++ rb-norway-scraper/spec/requester-spec.js
@@ -1,35 +1,35 @@
 /*global require, describe, it, expect, jasmine*/
-(function() {
+(function () {
     'use strict';
-    describe("requiring requester", function() {
+    describe("requiring requester", function () {
         var requester = require("../lib/requester");
-        it("returns an object", function() {
+        it("returns an object", function () {
             expect(requester).toEqual(jasmine.any(Object));
         });
 
-        describe("and initing it", function() {
-            it("will throw an error if the object doesn't contain a urls array", function() {
-                expect(function() {
+        describe("and initing it", function () {
+            it("will throw an error if the object doesn't contain a urls array", function () {
+                expect(function () {
                     requester.init();
                 }).toThrowError("Need urls parameter");
             });
 
-            it("will throw an error if the parameter is not an array", function() {
-                expect(function() {
+            it("will throw an error if the parameter is not an array", function () {
+                expect(function () {
                     requester.init("hello");
                 }).toThrowError("Urls parameter needs to be an array");
-                expect(function() {
+                expect(function () {
                     requester.init(1);
                 }).toThrowError("Urls parameter needs to be an array");
-                expect(function() {
+                expect(function () {
                     requester.init({
                         foo: 1
                     });
                 }).toThrowError("Urls parameter needs to be an array");
             });
 
-            it("will return an object with a doneEach function", function(){
-                expect(function() {
+            it("will return an object with a doneEach function", function () {
+                expect(function () {
                     requester.init(["hello"].doneEach);
                 }).toBeTruthy();
             });
