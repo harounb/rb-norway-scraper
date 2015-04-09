3ebe66da9f3cd281299b4a8690fcc201a9d31a44
harounb <dekuharoun@gmail.com>
2015-04-09 13:01:26 +0100

    config.sample.json was not a valid json file
    Also testing the config with the still stub index.js

--- index.js
+++ index.js
@@ -1,2 +1,6 @@
-/*globals module, require*/
-module.exports = require('./lib/scraper');
+/*globals module, require, console*/
+module.exports = function rBNorwayScraper() {
+    'use strict';
+    var config = require("./config");
+    console.log(config);
+}();
