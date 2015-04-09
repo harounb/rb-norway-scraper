c8b9be55eaa3d02122e90d31d8c6d19d2285e19a
harounb <dekuharoun@gmail.com>
2015-04-09 11:56:41 +0100

    Added request-promise
    And made sure it returns a promise

--- spec/requester-spec.js
+++ spec/requester-spec.js
@@ -1,37 +1,47 @@
 /*global require, describe, it, expect, jasmine*/
-(function () {
+(function() {
     'use strict';
-    describe("requiring requester", function () {
-        var requester = require("../lib/requester");
-        it("returns an object", function () {
+
+    describe("requiring requester", function() {
+        var proxyquire = require('proxyquire').noCallThru(),
+            requester = proxyquire("../lib/requester", {
+                "request-promise": {
+                    get: function() {
+                        return {
+                            then: function() {
+
+                            }
+                        };
+                    }
+                }
+            });
+        it("returns an object", function() {
             expect(requester).toEqual(jasmine.any(Object));
         });
 
-        describe("and initing it", function () {
-            it("will throw an error if the object doesn't contain a urls array", function () {
-                expect(function () {
+        describe("and initing it", function() {
+            it("will throw an error if the object doesn't contain a urls array", function() {
+                expect(function() {
                     requester.init();
                 }).toThrowError("Need urls parameter");
             });
 
-            it("will throw an error if the parameter is not an array", function () {
-                expect(function () {
+            it("will throw an error if the parameter is not an array", function() {
+                expect(function() {
                     requester.init("hello");
                 }).toThrowError("Urls parameter needs to be an array");
-                expect(function () {
+                expect(function() {
                     requester.init(1);
                 }).toThrowError("Urls parameter needs to be an array");
-                expect(function () {
+                expect(function() {
                     requester.init({
                         foo: 1
                     });
                 }).toThrowError("Urls parameter needs to be an array");
             });
 
-            it("will return an object with a doneEach function", function () {
-                expect(function () {
-                    requester.init(["hello"].doneEach);
-                }).toBeTruthy();
+            it("will return an object with a then function", function() {
+                expect(requester.init(["hello"]).then).toBeTruthy();
             });
 
         });
