e57e85d694c60a3aefc77f771b4fb2233aa3fffe
harounb <dekuharoun@gmail.com>
2015-05-28 12:08:48 +0100

    configurations file can now be specified in args

--- index.js
+++ index.js
@@ -1,11 +1,46 @@
-/*globals module, require*/
+/*globals module, require, process, console*/
 module.exports = (function rBNorwayScraper() {
     'use strict';
 
     //Dependencies
-    var config = require("./config"),
-        scraper = require("./lib/scraper");
+    var scraper = require("./lib/scraper"),
+        jf = require('jsonfile'),
 
-    scraper.scrape(config);
+        //functions
+        isJSON,
+
+        //variables
+        file;
+
+    /**
+        check if string is parseable to json.
+        Source: http://stackoverflow.com/questions/9804777/how-to-test-if-a-string-is-json-or-not
+    */
+    isJSON = function isJSON(str) {
+        try {
+            JSON.parse(str);
+        } catch (e) {
+            return false;
+        }
+        return true;
+    };
+
+    if (!isJSON(process.argv[2])) {
+        if (process.argv[2]) {
+            console.log("Provided argument not JSON. Interpreting as file");
+            file = process.argv[2];
+        } else {
+            console.log("No argument provided. Attempting to load config from config.json");
+            file = "config.json";
+        }
+        jf.readFile(file, function (err, obj) {
+            scraper.scrape(obj);
+        });
+
+    } else {
+        console.log("provided argument is JSON. Using as config");
+        scraper.scrape();
+
+    }
 
 }());
